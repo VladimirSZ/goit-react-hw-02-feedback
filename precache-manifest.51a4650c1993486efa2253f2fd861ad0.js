36f2c2dffb07d930229a975f5d26f48a20f2183f
VladimirSZ <zyryanov.vladimir.s@gmail.com>
2020-05-18 00:21:07 +0300

    Updates

---
+++ precache-manifest.51a4650c1993486efa2253f2fd861ad0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d919642efca83495d15644a220a4a4",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "659d995d80a58fc65245",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9c8ae13a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9c8ae13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0bf0383d653a17ead1",
+    "url": "/goit-react-hw-02-feedback/static/js/main.79feeea5.chunk.js"
+  },
+  {
+    "revision": "530a2ea63a63b985e80c",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.f97b67d2.js"
+  }
+]);
